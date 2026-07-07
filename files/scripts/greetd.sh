9ce904a3a02244e08d8caa8949a2f6f2be51f3c6
Fyllus <fylluszophia@proton.me>
2026-07-07 10:47:37 -0300

    fix: greetd configs

--- files/scripts/greetd.sh
+++ files/scripts/greetd.sh
@@ -4,6 +4,3 @@ set -oeux pipefail
 systemctl disable gdm.service || true
 systemctl disable sddm.service || true
 systemctl enable greetd.service
-mkdir -p /var/cache/greetd
-chown -R greeter:greeter /var/cache/greetd
-chmod 0755 /var/cache/greetd
